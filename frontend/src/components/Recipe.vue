b58918bc36c5b0cee2b380a8d2fd0503629c03d1
Rémi Even <remi.even@zenika.com>
2019-02-07 23:27:19 +0100

    added markdown for recipe howto

--- frontend/src/components/Recipe.vue
+++ frontend/src/components/Recipe.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <h1>{{ recipe.name }}</h1>
-    <p>{{ recipe.howTo }}</p>
+    <h2>Ingrédients</h2>
     <ul>
       <li v-for="ingredient in recipe.ingredients" :key="ingredient.id">
         {{ingredient.name}} {{ingredient.quantity ? `(${ingredient.quantity})` : ''}}
       </li>
     </ul>
+    <h2>Instructions</h2>
+    <div v-html="compiledHowTo" />
   </div>
 </template>
 
 <script>
+import marked from 'marked'
+
 import recipeApi from '@/api/recipe'
 
 export default {
@@ -26,12 +30,15 @@ export default {
   computed: {
     recipe() {
       return this.$store.state.recipe
-    }
+    },
+    compiledHowTo() {
+      return marked((this.$store.state.recipe || {}).howTo)
+    },
   },
   methods: {
     getRecipe(recipeId) {
       this.$store.dispatch('setRecipe', {recipeId})
-    }
+    },
   }
   // FIXME: https://router.vuejs.org/guide/essentials/dynamic-matching.html#reacting-to-params-changes
 }
